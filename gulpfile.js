1fe16fe0c9126435b23fff0532b709e59f3f080e
hujh <wx_a579eebac77948f8b27670cacf4d4737@git.code.tencent.com>
2019-10-08 11:46:06 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp')
+var series = gulp.series;
+var browserSync = require('browser-sync').create()
+var sass = require('gulp-sass');
+var reload = browserSync.reload;
+var px2rem = require('gulp-px2rem-plugin');
+
+sass.compiler = require('node-sass');
+
+
+// scss编译后的css将注入到浏览器里实现更新
+function scss() {
+  return gulp.src("assets/scss/*.scss")
+      .pipe(sass())
+      .pipe(px2rem({width_design: 750,'pieces':7.5,'ignore_px':[1,2]}))
+      .pipe(gulp.dest("assets/css"))
+      .pipe(reload({stream: true}));
+}
+
+
+function serve() {
+
+  browserSync.init({
+      server: "./"
+  });
+
+  gulp.watch("assets/scss/*.scss", scss);
+  gulp.watch("**/*.html", { events: 'change' }, reload);
+}
+
+exports.default = series(scss, serve)
