5841c727a65b0c9b8f833b3a1d4a308469187465
gongping1990 <wx_2d4ee0328b904aa789f1889f4721f6f3@git.code.tencent.com>
2019-10-10 10:45:09 +0800

    修改9

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,20 @@
   var $navBtn = $('.nav-icon')
   var $navLeft = $('.nav-left')
   var $navWrap = $('.nav-wrap')
-  var $btn = $('.btn')
+  var $btn1 = $('#clickBtn1')
+  var $btn2 = $('#clickBtn2')
   var $dialog = $('.dialog')
   var $dialogMask = $('.dialog-mask')
+  var $dialogContent = $('#dialogImg')
 
-  $btn.on('click', function() {
+  $btn1.on('click', function() {
     $dialog.addClass('show')
+    $dialogContent.attr('src', '../assets/image/home/gsw.jpg')
+  })
+
+  $btn2.on('click', function() {
+    $dialog.addClass('show')
+    $dialogContent.attr('src', '../assets/image/home/lxs.jpg')
   })
 
   $dialogMask.on('click', function(e) {
